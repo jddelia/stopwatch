bbf4955f7d4947aaf3db459f23897074461e5f43
jddelia <jddelia123@gmail.com>
2018-12-08 21:28:41 -0500

    Updates

---
+++ precache-manifest.38c1d67530d0afe0b23746368c623833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "650f3d7de6f8ae2fb848",
+    "url": "/stopwatch/static/js/runtime~main.650f3d7d.js"
+  },
+  {
+    "revision": "c7a3a76ac0fc6da2b236",
+    "url": "/stopwatch/static/js/main.c7a3a76a.chunk.js"
+  },
+  {
+    "revision": "0b52df4f42ccec3371a0",
+    "url": "/stopwatch/static/js/1.0b52df4f.chunk.js"
+  },
+  {
+    "revision": "c7a3a76ac0fc6da2b236",
+    "url": "/stopwatch/static/css/main.bd2519b8.chunk.css"
+  },
+  {
+    "revision": "43798a96e4243617a615c9b5c1df18d3",
+    "url": "/stopwatch/index.html"
+  }
+];
